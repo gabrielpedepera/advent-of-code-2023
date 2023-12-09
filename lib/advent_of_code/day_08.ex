e3e9afc2108449270d9843e45094afb58ed6a908
Gabriel Pereira <gabrielpedepera@gmail.com>
2023-12-09 00:06:06 +0000

    Resolve day8 part2

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -6,12 +6,15 @@ defmodule AdventOfCode.Day08 do
     |> Camel.solve()
   end
 
-  def part2(_args) do
+  def part2(input \\ @input) do
+    input
+    |> Camel.parse()
+    |> Camel.solve_part2()
   end
 end
 
 defmodule Camel do
-  defstruct [:steps, :network, :current, :step_index, :steps_count]
+  defstruct [:steps, :network, :current, :step_index, :steps_count, :visited]
 
   def solve(input) do
     %{steps: steps, network: network} = input
@@ -41,7 +44,7 @@ defmodule Camel do
       Enum.map(network, fn {k, v} -> {k, %{"L" => Enum.at(v, 0), "R" => Enum.at(v, 1)}} end)
       |> Enum.into(%{})
 
-    %{steps: steps, network: network}
+    %Camel{steps: steps, network: network}
   end
 
   defp bfs(%Camel{current: "ZZZ", steps_count: steps_count}), do: steps_count
@@ -56,5 +59,48 @@ defmodule Camel do
         step_index: state.step_index + 1,
         steps_count: state.steps_count + 1
     })
+  end
+
+  def solve_part2(input) do
+    input
+    |> get_pattern_for_starting_nodes()
+    |> get_least_common_multiple()
+  end
+
+  defp get_pattern_for_starting_nodes(%{network: network} = state) do
+    network
+    |> Map.keys()
+    |> Enum.filter(&String.ends_with?(&1, "A"))
+    |> Enum.map(fn start_node ->
+      steps_to_final_node(%{state | current: start_node, step_index: 0, steps_count: 0})
+    end)
+  end
+
+  defp steps_to_final_node(
+         %Camel{steps: instructions, network: network, current: current_node} = state,
+         current_steps \\ 0
+       ) do
+    if String.ends_with?(current_node, "Z") do
+      current_steps
+    else
+      index = rem(current_steps, length(instructions))
+      instruction = Enum.at(instructions, index)
+      new_node = Map.get(network, current_node)[instruction]
+
+      steps_to_final_node(%{state | current: new_node}, current_steps + 1)
+    end
+  end
+
+  defp get_least_common_multiple(numbers) do
+    leading_number = Enum.max(numbers)
+    get_least_common_multiple(numbers, leading_number, leading_number)
+  end
+
+  defp get_least_common_multiple(numbers, leading_number, multiple) do
+    if Enum.all?(numbers, &(rem(multiple, &1) === 0)) do
+      multiple
+    else
+      get_least_common_multiple(numbers, leading_number, multiple + leading_number)
+    end
   end
 end
--- test/advent_of_code/day_08_test.exs
+++ test/advent_of_code/day_08_test.exs
@@ -9,12 +9,10 @@ defmodule AdventOfCode.Day08Test do
     assert 6 == result
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    result = part2(input2())
 
-    assert result
+    assert 6 == result
   end
 
   def input do
@@ -24,6 +22,21 @@ defmodule AdventOfCode.Day08Test do
     AAA = (BBB, BBB)
     BBB = (AAA, ZZZ)
     ZZZ = (ZZZ, ZZZ)
+    """
+  end
+
+  def input2 do
+    """
+    LR
+
+    11A = (11B, XXX)
+    11B = (XXX, 11Z)
+    11Z = (11B, XXX)
+    22A = (22B, XXX)
+    22B = (22C, 22C)
+    22C = (22Z, 22Z)
+    22Z = (22B, 22B)
+    XXX = (XXX, XXX)
     """
   end
 end
